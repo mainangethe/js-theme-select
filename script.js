d65f834e31f0ebb7bc56f581e1d08ad72685e2dd
mainangethe <ngethe@mradi.co.ke>
2019-01-20 16:44:18 +0300

    Add Scripting to Index
    Added the dynamic changing of the foreground and background colours
    using javascript.
    
    Uses the ternary operator to decide values to pass to the update
    function.

---
+++ script.js
@@ -0,0 +1,19 @@
+const page_html = document.querySelector('html');
+const page_select = document.querySelector('select');
+
+function colorUpdate( bg_color, fg_color) {
+  page_html.style.background = bg_color;
+  page_html.style.color = fg_color;
+  page_select.disabled = true;
+}
+
+futureDetermination = function() {
+  let user_choice = page_select.value;
+
+  (user_choice === 'dark') ? colorUpdate(  'linear-gradient(-225deg, #000 0%, #C30032 52%, #F11B52 100%)' , 'black'): colorUpdate( 'linear-gradient(-225deg, #22E1FF 0%, #1D8FE1 48%, #625EB1 100%)', 'white');
+}
+
+//page_select.onchange = function() {
+  //( page_select.value === 'dark' ) ? colorUpdate('black', 'red') : colorUpdate('blue', 'white');
+//}
+page_select.addEventListener('change', futureDetermination);
